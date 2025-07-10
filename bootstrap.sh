7a512f74339c0b36db95a7e664801ab947979d2e
Robert Roach <rjayroach@gmail.com>
2025-07-10 01:08:35 -0400

    update ansible version

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,10 @@
 # bootstrap.sh
 set -e
 
-# Install ansible
+# Install ansible using the PPA method
 sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible git
 
 # Create ansible user with authorized key and passwordless sudo
@@ -11,17 +13,19 @@ if ! id -u ansible &>/dev/null; then
     sudo useradd -m -s /bin/bash ansible
     echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
     sudo -u ansible mkdir -p /home/ansible/.ssh
+    # TODO: The key is not yet available b/c we haven't cloned the repo
     sudo cp authorized_keys /home/ansible/.ssh/authorized_keys
     sudo chown ansible:ansible /home/ansible/.ssh/authorized_keys
     sudo chmod 700 /home/ansible/.ssh
     sudo chmod 600 /home/ansible/.ssh/authorized_keys
-    sudo -u ansible mkdir -p /home/ansible/.config /home/ansible/.local/bin /home/ansible/.local/share /home/ansible/.local/state /home/ansible/.cache
 fi
 
 # Clone roles repo (if not already present)
-if [ ! -d "/etc/ansible/roles" ]; then
-    sudo -u ansible mkdir -p /etc/ansible
-    sudo -u ansible git clone git@github.com:maxcole/ansible.git /etc/ansible/roles
+if [ ! -d "/usr/share/ansible/collections" ]; then
+    sudo mkdir -p /usr/share/ansible/collections/ansible_collections
+    sudo chown:chown ansible /usr/share/ansible -R
+    sudo -u ansible git clone git@github.com:maxcole/ansible.git \
+      /usr/share/ansible/collections/ansible_collections/rjayroach
 fi
 
 # Clone bootstrap repo to ansible user's home directory (if not already present)
